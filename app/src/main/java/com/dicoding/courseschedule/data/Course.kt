d8f1f685c1f951f909064bb8db3f54b74e138a14
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 23:06:24 +0700

    Fixing TODO 1

--- app/src/main/java/com/dicoding/courseschedule/data/Course.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/Course.kt
@@ -10,25 +10,12 @@ import androidx.room.PrimaryKey
 
 data class Course(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo (name = "id")
     val id :Int = 0 ,
-
-    @ColumnInfo(name = "courseName")
     val courseName : String ,
-
-    @ColumnInfo( name = "day")
     val day : Int ,
-
-    @ColumnInfo(name = "startTime")
     val startTime : String ,
-
-    @ColumnInfo (name  = "endTime")
     val endTime : String  ,
-
-    @ColumnInfo (name = "lecturer")
     val lecturer  : String ,
-
-    @ColumnInfo (name = "note")
     val note  :String
 )
 
