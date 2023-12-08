f6c4fa72f0cf4dc2c668a2b2dba8c2ef4eb70baa
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 23:07:29 +0700

    Fixing TODO 4

--- app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
@@ -3,37 +3,59 @@ package com.dicoding.courseschedule.data
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.dicoding.courseschedule.util.QueryType
+import com.dicoding.courseschedule.util.QueryUtil
 import com.dicoding.courseschedule.util.QueryUtil.nearestQuery
+import com.dicoding.courseschedule.util.QueryUtil.sortedQuery
 import com.dicoding.courseschedule.util.SortType
 import com.dicoding.courseschedule.util.executeThread
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 //TODO 4 : Implement repository with appropriate dao
 class DataRepository(private val dao: CourseDao) {
 
     fun getNearestSchedule(queryType: QueryType) : LiveData<Course?> {
-        throw NotImplementedError("needs implementation")
+        val query : SupportSQLiteQuery = QueryUtil.nearestQuery(type = queryType)
+        return dao.getNearestSchedule(query)
     }
 
     fun getAllCourse(sortType: SortType): LiveData<PagedList<Course>> {
-        throw NotImplementedError("needs implementation")
+        val query = sortedQuery(sortType)
+        val courseList = dao.getAll(query)
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(PAGE_SIZE)
+            .setEnablePlaceholders(false)
+            .setInitialLoadSizeHint(PAGE_SIZE)
+            .build()
+        return  LivePagedListBuilder(courseList , config) . build()
     }
 
     fun getCourse(id: Int) : LiveData<Course> {
-        throw NotImplementedError("needs implementation")
+        return  dao.getCourse(id)
     }
 
     fun getTodaySchedule() : List<Course> {
-        throw NotImplementedError("needs implementation")
+        val dayOfWeek = GregorianCalendar().get(Calendar.DAY_OF_WEEK)
+
+        var adjustedDayOfWeek = dayOfWeek
+        if (adjustedDayOfWeek == 0) {
+            adjustedDayOfWeek = 7
+        }
+
+        return dao.getTodaySchedule(adjustedDayOfWeek)
     }
 
     fun insert(course: Course) = executeThread {
-
+        dao.insert(course)
     }
 
     fun delete(course: Course) = executeThread {
-
+        dao.delete(course)
     }
 
     companion object {
