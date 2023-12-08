9a61310a565256e405ac30192ba84fe04a4751bf
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 22:22:06 +0700

    TODO 1
    //TODO 1 : Define a local database table using the schema in app/schema/course.json

--- app/src/main/java/com/dicoding/courseschedule/data/Course.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/Course.kt
@@ -1,12 +1,41 @@
 package com.dicoding.courseschedule.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //TODO 1 : Define a local database table using the schema in app/schema/course.json
+
+@Entity(tableName = "course")
+
 data class Course(
-    val id: Int = 0,
-    val courseName: String,
-    val day: Int,
-    val startTime: String,
-    val endTime: String,
-    val lecturer: String,
-    val note: String
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo (name = "id")
+    val id :Int = 0 ,
+
+    @ColumnInfo(name = "courseName")
+    val courseName : String ,
+
+    @ColumnInfo( name = "day")
+    val day : Int ,
+
+    @ColumnInfo(name = "startTime")
+    val startTime : String ,
+
+    @ColumnInfo (name  = "endTime")
+    val endTime : String  ,
+
+    @ColumnInfo (name = "lecturer")
+    val lecturer  : String ,
+
+    @ColumnInfo (name = "note")
+    val note  :String
 )
+
+//    val id: Int = 0,
+//    val courseName: String,
+//    val day: Int,
+//    val startTime: String,
+//    val endTime: String,
+//    val lecturer: String,
+//    val note: String
