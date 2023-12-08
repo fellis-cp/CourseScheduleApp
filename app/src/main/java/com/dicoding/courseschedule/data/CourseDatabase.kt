3919f58f860399ff2085f642f4df3d1d1b227f2b
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 23:07:10 +0700

    Add INSTANCE

--- app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
@@ -6,22 +6,27 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 //TODO 3 : Define room database class
-
 @Database ( version = 1 , entities = [Course::class] , exportSchema = false)
 abstract class CourseDatabase : RoomDatabase() {
+
+
     abstract fun courseDao(): CourseDao
 
     companion object {
 
         @Volatile
-        private var instance: CourseDatabase? = null
+        private var INSTANCE: CourseDatabase? = null
 
         fun getInstance(context: Context): CourseDatabase {
-            return synchronized(this){
-                instance ?: Room.databaseBuilder(context, CourseDatabase::class.java, "courses.db")
-                        .build()
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, CourseDatabase::class.java,
+                    "courses.db"
+                ).build()
+                INSTANCE = instance
+                instance
             }
-        }
 
+        }
     }
 }
