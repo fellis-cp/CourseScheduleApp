270d00ab156551dfd0794d5636994c68d986029a
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-09 00:18:38 +0700

    Adding nearest func

--- app/src/main/java/com/dicoding/courseschedule/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/home/HomeViewModel.kt
@@ -1,16 +1,24 @@
 package com.dicoding.courseschedule.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.data.DataRepository
 import com.dicoding.courseschedule.util.QueryType
 
 class HomeViewModel(repository: DataRepository): ViewModel() {
 
+    private val repo = repository
+
     private val _queryType = MutableLiveData<QueryType>()
 
     init {
         _queryType.value = QueryType.CURRENT_DAY
+    }
+
+    fun nearest (queryType: QueryType) : LiveData<Course?> {
+        return repo.getNearestSchedule(queryType)
     }
 
     fun setQueryType(queryType: QueryType) {
